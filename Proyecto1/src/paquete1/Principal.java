d77b33d6055d274d6a444e97c070ef6247ce9089
diurno <diurno@B21>
2024-01-11 13:26:55 +0100

    Cambio2

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,5 +3,6 @@ package paquete1;
 public class Principal {
 	public static void main(String[] args) {
 			System.out.println("Ejemplo de control de versiones con Git");
+			System.out.println("Segundo ejemplo de control de versiones con Git");
 	}
 }
