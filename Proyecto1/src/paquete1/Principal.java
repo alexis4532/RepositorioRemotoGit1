7cff0390254a5e3711a4dd39ce85b77ca7414d6e
diurno <diurno@B21>
2024-01-12 10:46:40 +0100

    Rama1 Subida

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 			System.out.println("Ejemplo de control de versiones con Git");
 			System.out.println("Segundo ejemplo de control de versiones con Git");
+			System.out.println("Cambio nuevo");
 	}
 }
